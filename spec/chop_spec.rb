4d57d9e097ce2f860ff72fc5d07ed8ce5ea9de1a
dscamp <dscamp@gmail.com>
2013-05-27 10:31:17 -0300

    missed the .should in spec!

--- spec/chop_spec.rb
+++ spec/chop_spec.rb
@@ -10,16 +10,24 @@ describe "binary chop" do
     end
     
     it "returns -1 when not in array" do
-      chop(3,[1])
+      chop(3,[1]).should eq(-1)
     end
 
     it "returns 0 when only number in array" do
       chop(3,[3])
     end
-
   end
 
-  
+#  context "slightly larger array" do
+#    before(:each) do
+#      nums = [1,3,5]
+#    end
+
+#    it "should return 0 when item in array" do
+#      chop(1,nums)
+#    end
+    
+#  end
   
   
 
