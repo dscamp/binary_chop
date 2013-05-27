80cc073e7b9fb68c4ceae13eb1adecdf1ab19c25
dscamp <dscamp@gmail.com>
2013-05-27 10:26:43 -0300

    simple check finding only one item in array.

--- spec/chop_spec.rb
+++ spec/chop_spec.rb
@@ -4,13 +4,17 @@ require_relative '../lib/chop'
 
 describe "binary chop" do
 
-  context "number is not in array" do
+  context "some simple checks" do
     it "returns -1 when array empty" do
       chop(3, []).should eq(-1)
     end
     
-    it "is -1 when not in array" do
+    it "returns -1 when not in array" do
       chop(3,[1])
+    end
+
+    it "returns 0 when only number in array" do
+      chop(3,[3])
     end
 
   end
