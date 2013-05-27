6c1822997d87f92af6a947ef3e342a3527a77976
dscamp <dscamp@gmail.com>
2013-05-27 10:23:29 -0300

    check array with one element that is not the one being searched for

--- spec/chop_spec.rb
+++ spec/chop_spec.rb
@@ -5,12 +5,13 @@ require_relative '../lib/chop'
 describe "binary chop" do
 
   context "number is not in array" do
-
-    
     it "returns -1 when array empty" do
       chop(3, []).should eq(-1)
     end
     
+    it "is -1 when not in array" do
+      chop(3,[1])
+    end
 
   end
 
