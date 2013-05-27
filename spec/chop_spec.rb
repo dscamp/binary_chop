69802cd991eca5f3edf397b71e63c969aa1c7f63
dscamp <dscamp@gmail.com>
2013-05-27 11:58:38 -0300

    implemented binary chop algorithm

--- spec/chop_spec.rb
+++ spec/chop_spec.rb
@@ -4,7 +4,7 @@ require_relative '../lib/chop'
 
 describe "binary chop" do
 
-  context "some simple checks" do
+  context "some simple checks with array of none or one" do
     it "returns -1 when array empty" do
       chop(3, []).should eq(-1)
     end
@@ -18,7 +18,7 @@ describe "binary chop" do
     end
   end
 
-  context "when using multi number array" do
+  context "when using 3 digit array" do
     before(:each) do
       @nums = [1,3,5]
     end
@@ -26,18 +26,29 @@ describe "binary chop" do
     it "should return 0 when num is first item in array" do
       chop(1, @nums).should eq(0)
     end
-    
+
     it "should return 1 when num is  second item in array" do
       chop(3, @nums).should eq(1)
     end
 
-    it "should return -1 when num is greater that largest item in array" do
+
+    it "should return -1 when num is greater than largest item in array" do
       chop(7, @nums).should eq(-1)
     end
-
   end
   
   
+  context "when using a 5 digit array" do
+    before(:each) do
+      @nums = [1,3,5,7,9]
+    end
 
+    it "should return -1 when num is smaller than smallest number in array" do
+      chop(0,@nums).should be(-1)
+    end
+
+
+
+  end
 
 end
